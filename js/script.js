11cf020f9e216f32be3d83451773873163f237c6
Tyler van der Hoeven <hi@tyvdh.com>
2016-02-29 23:03:59 -0500

    added some fancy js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-$('.link').click(function(e) {
-	e.preventDefault();
-	var href = $.attr(this, 'href').replace('/', '');
-  $('html, body').animate({
-    scrollTop: $('.'+ href).offset().top - 20
-  }, 500);
-});	
+$(function() {
+  $('.menu a').click(function(event) {
+    event.preventDefault();
+    var string = $(this).data('string');
+
+    if ($(string).length) {
+      $('html, body').animate({scrollTop: $(string).offset().top - 20}, 300);
+    } else {
+      console.warn('Element does not exist');
+    }
+  });
+
+  $('input[name="count"]').keyup(function() {
+    $('.error').remove();
+
+    var letters = $(this).val().replace(/[\d]/g, '');
+
+    if (letters.length) {
+      $(this).after('<span class="error">Only numbers please!</span>');
+    }
+
+    var clean = $(this).val().replace(/[\D]/g, '');
+    $(this).val(clean);
+  });
+});
