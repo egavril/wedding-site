9133dc75465411831910f54bb795ed1af5dbce26
egavril <e.gavrilyuk@gmail.com>
2016-02-29 18:48:06 -0800

    still trying to get the smooth scroll to work + minor changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-$('.link').click(function(e) {
+$('#rsvp').click(function(e) {
 	e.preventDefault();
 	var href = $.attr(this, 'href').replace('/', '');
-  $('html, body').animate({
-    scrollTop: $('.'+ href).offset().top - 20
-  }, 500);
+ 	$('html, body').animate({scrollTop: $('.'+ href).offset().top - 20}, 500);
 });	
+
+/*
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+*/
